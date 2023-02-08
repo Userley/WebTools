73ca77d0074adf400fa3211c7fda772bbf791bcf
Erick Leyva Díaz <eleyva@wts.com.pe>
2023-02-08 10:15:33 -0500

    Se carga registro de tabla contactos de BD webtools

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contactos;
 use Illuminate\Http\Request;
 
 class ContactosController extends Controller
 {
-    public function index(){
-        return view('contactos.contactos');
+    public function index()
+    {
+
+        $contactos = Contactos::all();
+
+        // return $contactos;
+        return view('contactos.contactos',compact('contactos'));
     }
 
-    public function crear(){
+    public function crear()
+    {
         return view('contactos.crear');
     }
 
-    public function detalle(){
+    public function detalle()
+    {
         return view('contactos.detalle');
     }
 
